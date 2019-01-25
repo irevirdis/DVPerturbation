2617dff51ce3d1a010b76d0aabed416283ab6f93
irevirdis <ire.virdis@gmail.com>
2019-01-25 15:56:42 +0100

    step2

--- DVPerturbation/hybridSlicer.py
+++ DVPerturbation/hybridSlicer.py
@@ -22,6 +22,14 @@ class HybridSlicer(object):
         # check part
         with open('draft_of_hybrid.txt', 'w') as d:
             d.write(str(matrix1))
+        with open('draft_of_hybrid.txt', 'r') as r:
+            with open('coordinates_matrix.txt', 'w') as w:
+                data = r.read()
+                data = data.replace("[","")
+                data = data.replace("]","")
+                data = data.replace("'", "")
+                w.write(data)
+        #os.system("rm draft_of_hybrid.txt")
 
         # PART 2: 
         # section MARKER_ELEMS: c0 = connectivity, c1=node1, c2=node2, c3=node3, c4=optional|quad cells
@@ -31,6 +39,7 @@ class HybridSlicer(object):
         rows = [x for x in open(self.mesh).readlines()] 
         dimension=0
         limit=0
+
         for i in range(len(rows)):
             if 'MARKER_TAG' and self.surface in rows[i]:
                 #print 'the row is:', rows[int(i+1)]
@@ -42,29 +51,50 @@ class HybridSlicer(object):
                 #print 'the last line that will read is: (in python index)', upper_limit
                 elem = rows[int(i+2)].split()
                 dimension = int(len(elem)-1)
-                print 'the number of the columns is:', dimension
-                print 'connection is:', 
-        #print 'sart:', start_read 
-        #print 'upper:',upper_limit 
-        print 'start_read list is:', (start_read)
-        print 'upper limit list is:', (upper_limit)
-        print 'limit', limit, 'dimension', dimension
+
+        dim_m = len(matrix1)
+        dim_c = int(upper_limit[0] - start_read[0])
+        skip  = int(dim_c - dim_m)
         connection = np.zeros((limit, int(4*dimension)))
-        for i in range(len(rows)): 
-            if (i>= start_read[0]) and (i<=upper_limit[0]):
+
+        for i in range(len(rows)):
+            #m_rows=len(matrix1)
+            if (i>= start_read[0]) and (i< (int(upper_limit[0]))):
                 elem = rows[i].split()
                 for j in range(int (len(elem)-1)):
-                    print 'the elem is:' , elem[int(j+1)]
+                    #print 'the elem is:' , elem[int(2*(j+1))]
                     index = int(i-start_read[0])
-                    print 'the index is:', index
-                    print 'the elem j+1 is:', elem[j+1]
+                    #print 'the index is:', index
+                    #print 'the elem j+1 is:', elem[j+1]
                     connection[index,j] = elem[j+1]
+                    # part dedicated to the x, y, z  coordiantes:
+                    linked_row = connection[index,j]
+                    c_index = int(3*j + len(elem)-1)
+                    c_index_x = c_index
+                    c_index_y = int(c_index + 1)
+                    c_index_z = int(c_index + 2)
+                    
+                    for k in range(len(matrix1)):
+                        if connection[index,j] == float(matrix1[k,0]):
+                            connection[index, c_index_x] = matrix1[k,1]
+                            connection[index, c_index_y] = matrix1[k,2]
+                            connection[index, c_index_z] = matrix1[k,3]
+       
         with open('draft_connections.txt', 'w') as d:
-            d.write(str(connection))
+            np.savetxt('draft_connections.txt', connection, fmt='%.6e')
+            #d.write(str(connection))
+        with open('draft_connections.txt', 'r') as r:
+            with open('connections_matrix.txt', 'w') as w:
+                data = r.read()
+                data = data.replace("[","")
+                data = data.replace("]","")
+                data = data.replace("'", "")
+                w.write(data)
+        #os.system("rm draft_connections.txt")
 
         os.system("mkdir "+str(self.class_name+"_OUTPUTs"))
         description = 'this class is hybridslicer; this doc must be completed'
-        involved_outputs=np.array(['draft_of_hybrid.txt', 'draft_connections.txt'])
+        involved_outputs=np.array(['coordinates_matrix.txt', 'connections_matrix.txt', 'draft_of_hybrid.txt', 'draft_connections.txt'])
         invoked_method = 'init'
         collection = CollectResults(class_name=self.class_name, involved_outputs=involved_outputs, invoked_method=invoked_method, description=description)    
         collection.Collect()
